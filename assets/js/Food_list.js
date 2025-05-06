e2db9d12c6afe4f5d986ea2e305277362397ef7c
Đinh Quang Tuấn Anh <dqtanh.1123.cv@gmail.com>
2025-05-06 13:10:47 +0700

    Update Food_list.js

--- assets/js/Food_list.js
+++ assets/js/Food_list.js
@@ -2,7 +2,7 @@ import { params } from "./params.js";
 import { fetchApi, menuProduct } from "./variable.js";
 
 export function Food__List(){
-    fetchApi(`./database/db.json/products?category=${params.category}`)
+    fetchApi(`./database/db.json?category=${params.category}`)
     .then(data => {
         let htmls = data.map(item => {
             return `<div class="product__item"><div class="product__image"><img src="${item.image}" alt=""></div>
